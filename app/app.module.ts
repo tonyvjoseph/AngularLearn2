2636ed4533c2d75a3d4ed51eee69910f6848853b
tonyvjoseph <tonyvjoseph@gmail.com>
2019-05-16 13:27:39 +0530

    web api related changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { EmployeeListComponent } from './employee/employee-list/employee-list.co
 
 import { EmployeeTitle } from './employee/employee-title.pipe';
 import { EmployeeCountComponent } from './employee/employee-count/employee-count.component';
-
+import { OthersComponent } from './others/others.component';
 
 
 @NgModule({
@@ -18,13 +19,15 @@ import { EmployeeCountComponent } from './employee/employee-count/employee-count
     EmployeeComponent,
     EmployeeListComponent,
     EmployeeTitle,
-    EmployeeCountComponent
+    EmployeeCountComponent,
+    OthersComponent
         
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
